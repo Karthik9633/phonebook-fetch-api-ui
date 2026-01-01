8515d3cbd7edba8ce0be0e3c3e057cb8e944339c
Karthik B <karthikbalu5533@gmail.com>
2026-01-01 21:34:44 +0530

    Implement add contact functionality with validation

--- javaScript/script.js
+++ javaScript/script.js
@@ -10,4 +10,30 @@ let contacts = [];
 async function loadContacts() {
   const response = await fetch("c.json");
   contacts = await response.json();
+  showContacts(contacts)
+}
+
+function showContacts(list) {
+  grid.innerHTML = "";
+
+  if (list.length === 0) {
+    grid.innerHTML = `<p class="empty">No contacts found</p>`;
+    return;
+  }
+
+  list.forEach(contact => {
+    const card = document.createElement("div");
+    card.className = "card";
+
+    card.innerHTML = `
+      <h4>${contact.name}</h4>
+      <p>${contact.phone}</p>
+      <div class="actions">
+        <button class="edit">Edit</button>
+        <button class="delete">Delete</button>
+      </div>
+    `;
+
+   
+  });
 }
